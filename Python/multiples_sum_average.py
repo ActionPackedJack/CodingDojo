4eeae12b36602cbedb40f525eaae48c6f1b5694c
ActionPackedJack <actionpackedjack@gmail.com>
2018-04-02 15:09:07 -0500

    Code for averages, sums, and factors.

---
+++ Python/multiples_sum_average.py
@@ -0,0 +1,14 @@
+for i in range(1001):
+    if i%2==1:
+        print i
+
+for i in range(1000001):
+    if i%5==0:
+        print i
+
+a = [1, 2, 5, 10, 255, 3]
+sum=0
+for i in range(len(a)):
+    sum+=a[i]
+print sum
+print (sum/len(a))
