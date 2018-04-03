737af2d1ef5202d49e924072e3e30635d3e8810c
ActionPackedJack <actionpackedjack@gmail.com>
2018-04-03 13:28:27 -0500

    Function for logging the tuples of a dictionary.

---
+++ Python/extrapolate_dictionary.py
@@ -0,0 +1,7 @@
+me={"name": "Honorable General Toshiro Akahana", "country of birth":"the Forbidden Island of the Three Floating Suns", "age": "unintelligble to your small-minded perception of time"}
+
+def extrapolate_dictionary(dict):
+    for key,data in dict.items():
+        print "My "+key+" is "+ data + "."
+
+extrapolate_dictionary(me)
