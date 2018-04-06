7c496af97a87adebfbb9732bd71ab0d4fc5da09d
ActionPackedJack <actionpackedjack@gmail.com>
2018-04-05 19:11:35 -0500

    Flask project with multiple pages, a form, post method, and a static folder.

---
+++ Python/myEnvironments/flaskEnv/landing_page/landing_page.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def openpage():
+    return render_template('index.html')
+
+@app.route('/ninja')
+def openProjects():
+    return render_template('ninja.html')
+
+@app.route('/dojos/new')
+def openAbout():
+    return render_template('dojos.html')
+
+app.run(debug=True)
