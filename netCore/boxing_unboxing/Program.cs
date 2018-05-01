d0965c66a2d945bd579a3fee4a2d4f9e9ecd3588
ActionPackedJack <actionpackedjack@gmail.com>
2018-05-01 14:48:27 -0500

    C# code for list containing different types of data.

---
+++ netCore/boxing_unboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> values = new List<object>();
+            values.Add(7);
+            values.Add(-1);
+            values.Add(28);
+            values.Add(true);
+            values.Add("chair");
+            int sum = 0;
+            foreach(object obj in values)
+            {
+                System.Console.WriteLine(obj);
+                if (obj is int)
+                {
+                    sum += Convert.ToInt32(obj);
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
