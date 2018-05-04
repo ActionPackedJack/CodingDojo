ccf7de30d0a37abc97de1d78da8907298f92ed79
ActionPackedJack <actionpackedjack@gmail.com>
2018-05-04 15:01:19 -0500

    App that captures parameters from a URL and builds a JSON object.

---
+++ netCore/calling_card/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using calling_card.Models;
+
+namespace calling_card.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("{first}/{last}/{age}/{color}")]
+        public JsonResult DisplayInt(string first, string last, string age, string color)
+        {
+            var CallingCard = new {
+                First_name = first,
+                Last_name = last,
+                Age = age,
+                Favorite_color= color
+            };
+            return Json(CallingCard);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
