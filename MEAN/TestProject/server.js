55d22afd66199c143273ea39ce8b0de470234666
ActionPackedJack <actionpackedjack@gmail.com>
2018-06-08 14:38:12 -0500

    Express project that implements session data.

---
+++ MEAN/TestProject/server.js
@@ -0,0 +1,43 @@
+// require express
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+var session = require('express-session');
+// create the express app
+var app = express();
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+// setting up ejs and our views folder
+app.use(session({
+    secret: 'keyboardkitteh',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+    console.log(req.session.counter);
+    if(!req.session.counter){
+        console.log("NEW COUNTER")
+        req.session.counter=0;
+    }
+        req.session.counter++;
+        res.render("index", {counter: req.session.counter});
+    }   
+)
+// post route for adding a user
+app.post('/users', function(req, res) {
+ console.log("POST DATA", req.body);
+ // This is where we would add the user to the database
+ // Then redirect to the root route
+ res.redirect('/');
+})
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
